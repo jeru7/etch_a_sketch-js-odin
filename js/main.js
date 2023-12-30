97b2633f1dcca471924454427f34f135f749459e
jeru7 <john.ungab7@gmail.com>
2023-12-30 23:35:00 +0800

    js code setup done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// button elements
+const colorBtn = document.querySelector("#colorButton");
+const rgbBtn = document.querySelector("#rgbToggler");
+const neonBtn = document.querySelector("#neonToggler");
+const eraserBtn = document.querySelector("#eraserButton");
+const clearBtn = document.querySelector("#clearButton");
+const sizeBtn = document.querySelector("#sizeButton");
+
+// container elements
+const mainGridContainer = document.querySelector(".main__canvas__grids");
