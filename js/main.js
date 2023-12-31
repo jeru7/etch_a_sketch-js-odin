b2a3ff3895c5f88169788b397ecab9506f415f87
jeru7 <john.ungab7@gmail.com>
2023-12-31 16:46:36 +0800

    color picker modal toggler done

--- js/main.js
+++ js/main.js
@@ -5,6 +5,32 @@ const neonBtn = document.querySelector("#neonToggler");
 const eraserBtn = document.querySelector("#eraserButton");
 const clearBtn = document.querySelector("#clearButton");
 const sizeBtn = document.querySelector("#sizeButton");
+const closeBtn = document.querySelectorAll("#closeButton");
 
 // container elements
 const mainGridContainer = document.querySelector(".main__canvas__grids");
+const sizeSelectorModal = document.querySelector(".popUpSizeSelector");
+const colorPickerModal = document.querySelector(".popUpColorPicker");
+const backDrop = document.querySelector(".backdrop");
+
+// TODO: Functions for the buttons
+colorBtn.addEventListener("click", () => {
+  if (
+    colorPickerModal.style.display == "none" ||
+    colorPickerModal.style.display == ""
+  ) {
+    colorPickerModal.style.display = "flex";
+    backDrop.style.display = "block";
+  } else {
+    colorPickerModal.style.display = "none";
+    backDrop.style.display = "none";
+  }
+});
+
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const parentContainer = btn.parentNode;
+    parentContainer.style.display = "none";
+    backDrop.style.display = "none";
+  });
+});
